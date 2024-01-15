ce0b3e4fceb43b0c1cd0e73b23c584066e23eae3
SolidityDrone <pnn.claudoio@gmail.com>
2024-01-15 10:46:01 +0100

    added foundry folder

---
+++ contracts/Foundry/src/Account.sol
@@ -0,0 +1,219 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.11;
+
+/* solhint-disable avoid-low-level-calls */
+/* solhint-disable no-inline-assembly */
+/* solhint-disable reason-string */
+
+// Base
+import "@thirdweb/prebuilts/account/utils/BaseAccount.sol";
+
+// Extensions
+import "@thirdweb/prebuilts/account/utils/AccountCore.sol";
+import "@thirdweb/extension/upgradeable/ContractMetadata.sol";
+import "@thirdweb/external-deps/openzeppelin/token/ERC721/utils/ERC721Holder.sol";
+import "@thirdweb/external-deps/openzeppelin/token/ERC1155/utils/ERC1155Holder.sol";
+
+// Utils
+import "@thirdweb/eip/ERC1271.sol";
+import "@thirdweb/prebuilts/account/utils/Helpers.sol";
+import "@thirdweb/external-deps/openzeppelin/utils/cryptography/ECDSA.sol";
+import "@thirdweb/prebuilts/account/utils/BaseAccountFactory.sol";
+
+//   $$\     $$\       $$\                 $$\                         $$\
+//   $$ |    $$ |      \__|                $$ |                        $$ |
+// $$$$$$\   $$$$$$$\  $$\  $$$$$$\   $$$$$$$ |$$\  $$\  $$\  $$$$$$\  $$$$$$$\
+// \_$$  _|  $$  __$$\ $$ |$$  __$$\ $$  __$$ |$$ | $$ | $$ |$$  __$$\ $$  __$$\
+//   $$ |    $$ |  $$ |$$ |$$ |  \__|$$ /  $$ |$$ | $$ | $$ |$$$$$$$$ |$$ |  $$ |
+//   $$ |$$\ $$ |  $$ |$$ |$$ |      $$ |  $$ |$$ | $$ | $$ |$$   ____|$$ |  $$ |
+//   \$$$$  |$$ |  $$ |$$ |$$ |      \$$$$$$$ |\$$$$$\$$$$  |\$$$$$$$\ $$$$$$$  |
+//    \____/ \__|  \__|\__|\__|       \_______| \_____\____/  \_______|\_______/
+
+contract Account is AccountCore, ContractMetadata, ERC1271, ERC721Holder, ERC1155Holder {
+    using ECDSA for bytes32;
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    address public automationUpkeep;
+    address public defaultToken; // GHO
+    address public uniswapRouter;
+    bool public allowedSupply;
+    
+    bytes32 private constant MSG_TYPEHASH = keccak256("AccountMessage(bytes message)");
+
+    /*///////////////////////////////////////////////////////////////
+                    Constructor, Initializer, Modifiers
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(IEntryPoint _entrypoint, address _factory, address _defaultToken, address _uniswapRouter) AccountCore(_entrypoint, _factory) {
+        automationUpkeep = msg.sender;
+        defaultToken = _defaultToken; // GHO
+        uniswapRouter = _uniswapRouter; // Uniswap Router
+        allowedSupply = true;
+    }
+
+    /// @notice Checks whether the caller is the EntryPoint contract or the admin.
+    modifier onlyAdminOrEntrypoint() virtual {
+        require(msg.sender == address(entryPoint()) || isAdmin(msg.sender), "Account: not admin or EntryPoint.");
+        _;
+    }
+
+    /// @notice Checks whether the caller is the Chainlink Upkeep contract or the admin.
+    modifier onlyAdminOrUpkeep virtual {
+        require(msg.sender == automationUpkeep || isAdmin(msg.sender), "Account: not admin or Upkeep.");
+        _;
+    }
+
+
+    /// @notice Lets the account receive native tokens.
+    receive() external payable {}
+
+    /*///////////////////////////////////////////////////////////////
+                            View functions
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice See {IERC165-supportsInterface}.
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155Receiver) returns (bool) {
+        return
+            interfaceId == type(IERC1155Receiver).interfaceId ||
+            interfaceId == type(IERC721Receiver).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+
+    /// @notice See EIP-1271
+    function isValidSignature(
+        bytes32 _message,
+        bytes memory _signature
+    ) public view virtual override returns (bytes4 magicValue) {
+        bytes32 messageHash = getMessageHash(abi.encode(_message));
+        address signer = messageHash.recover(_signature);
+
+        if (isAdmin(signer)) {
+            return MAGICVALUE;
+        }
+
+        address caller = msg.sender;
+        EnumerableSet.AddressSet storage approvedTargets = _accountPermissionsStorage().approvedTargets[signer];
+
+        require(
+            approvedTargets.contains(caller) || (approvedTargets.length() == 1 && approvedTargets.at(0) == address(0)),
+            "Account: caller not approved target."
+        );
+
+        if (isActiveSigner(signer)) {
+            magicValue = MAGICVALUE;
+        }
+    }
+
+    /**
+     * @notice Returns the hash of message that should be signed for EIP1271 verification.
+     * @param message Message to be hashed i.e. `keccak256(abi.encode(data))`
+     * @return Hashed message
+     */
+    function getMessageHash(bytes memory message) public view returns (bytes32) {
+        bytes32 messageHash = keccak256(abi.encode(MSG_TYPEHASH, keccak256(message)));
+        return keccak256(abi.encodePacked("\x19\x01", _domainSeparatorV4(), messageHash));
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            External functions
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Executes a transaction (called directly from an admin, or by entryPoint)
+    function execute(address _target, uint256 _value, bytes calldata _calldata) external virtual onlyAdminOrEntrypoint {
+        _registerOnFactory();
+        _call(_target, _value, _calldata);
+    }
+
+    /// @notice Executes a sequence transaction (called directly from an admin, or by entryPoint)
+    function executeBatch(
+        address[] calldata _target,
+        uint256[] calldata _value,
+        bytes[] calldata _calldata
+    ) external virtual onlyAdminOrEntrypoint {
+        _registerOnFactory();
+
+        require(_target.length == _calldata.length && _target.length == _value.length, "Account: wrong array lengths.");
+        for (uint256 i = 0; i < _target.length; i++) {
+            _call(_target[i], _value[i], _calldata[i]);
+        }
+    }
+
+    /// @notice Special function execution for ghost wallet. Execute a swap for defaultToken and supply on AAVE.
+    function executeSwapAndSupply(
+        uint256 supplyAmount,
+        uint256 swapAmount,
+        address token,
+        address aavePool // AAVE Pool address to supply token different from defaultToken aka GHO
+    ) external virtual onlyAdminOrUpkeep {
+        _registerOnFactory();
+        require(allowedSupply, "Account: supply paused.");
+        // Supply token to AAVE
+        bytes memory supplyData = abi.encodeWithSelector(0x617ba037, token, supplyAmount, address(this), 0);
+        _call(aavePool, 0, supplyData);
+        // Swap token on Uniswap
+        address[] memory path = new address[](2);
+        path[0] = token;
+        path[1] = defaultToken; //GHO
+        bytes memory swapData = abi.encodeWithSelector(0x8803dbee, swapAmount, swapAmount, path, address(this), (block.timestamp+300));
+        _call(uniswapRouter, 0, swapData);
+    }
+
+    /// @notice Deposit funds for this account in Entrypoint.
+    function addDeposit() public payable {
+        entryPoint().depositTo{ value: msg.value }(address(this));
+    }
+
+    /// @notice Withdraw funds for this account from Entrypoint.
+    function withdrawDepositTo(address payable withdrawAddress, uint256 amount) public {
+        _onlyAdmin();
+        entryPoint().withdrawTo(withdrawAddress, amount);
+    }
+
+    function setUpkeep(address _upkeep) external virtual onlyAdminOrEntrypoint() {
+        automationUpkeep = _upkeep;
+    }
+    
+    function setDefaultToken(address _defaultToken) external virtual onlyAdminOrEntrypoint() {
+        defaultToken = _defaultToken;
+    }
+
+    function setUniswapRouter(address _uniswapRouter) external virtual onlyAdminOrEntrypoint() {
+        uniswapRouter = _uniswapRouter;
+    }
+
+    function setAllowedSupply(bool _allowedSupply) external virtual onlyAdminOrEntrypoint() {
+        allowedSupply = _allowedSupply;
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        Internal functions
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Registers the account on the factory if it hasn't been registered yet.
+    function _registerOnFactory() internal virtual {
+        BaseAccountFactory factoryContract = BaseAccountFactory(factory);
+        if (!factoryContract.isRegistered(address(this))) {
+            factoryContract.onRegister(AccountCoreStorage.data().creationSalt);
+        }
+    }
+
+    /// @dev Calls a target contract and reverts if it fails.
+    function _call(
+        address _target,
+        uint256 value,
+        bytes memory _calldata
+    ) internal virtual returns (bytes memory result) {
+        bool success;
+        (success, result) = _target.call{ value: value }(_calldata);
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+    }
+
+    /// @dev Returns whether contract metadata can be set in the given execution context.
+    function _canSetContractURI() internal view virtual override returns (bool) {
+        return isAdmin(msg.sender) || msg.sender == address(this);
+    }
+}
---
+++ contracts/Foundry/src/AccountFactory.sol
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
+
+// Utils
+import "@thirdweb/prebuilts/account/utils/BaseAccountFactory.sol";
+import "@thirdweb/prebuilts/account/utils/BaseAccount.sol";
+import "@thirdweb/external-deps/openzeppelin/proxy/Clones.sol";
+
+// Extensions
+import "@thirdweb/extension/upgradeable/PermissionsEnumerable.sol";
+import "@thirdweb/extension/upgradeable/ContractMetadata.sol";
+
+// Interface
+import "@thirdweb/prebuilts/account/interface/IEntrypoint.sol";
+
+// Smart wallet implementation
+import { Account } from "./Account.sol";
+
+// Chainlink automation interface 
+import "@chainlink/src/v0.8/automation/AutomationCompatible.sol";
+
+// Openzeppelin IERC20.sol
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract AccountFactory is BaseAccountFactory, ContractMetadata, PermissionsEnumerable, AutomationCompatible {
+
+    address internal owner;
+    address internal upkeep;
+    
+    address[] internal s_swappableERC20 = 
+    [
+        0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8,  //usdc
+        0x3e622317f8C93f7328350cF0B56d9eD4C620C5d6   //dai
+    ];
+
+    /*///////////////////////////////////////////////////////////////
+                            Constructor
+    //////////////////////////////////////////////////////////////*/
+    constructor(
+        address _defaultAdmin,
+        IEntryPoint _entrypoint,
+        address _defaultToken, // GHO
+        address _uniswapRouter // Uniswap Router
+    ) BaseAccountFactory(address(new Account(_entrypoint, address(this), _defaultToken, _uniswapRouter)), address(_entrypoint)) {
+        _setupRole(DEFAULT_ADMIN_ROLE, _defaultAdmin);
+    }
+    
+    function initializeUpkeep(address _upkeep) public onlyOwner() {
+        require(upkeep == address(0), "Already initialized");
+        require(msg.sender == owner, "Not owner");
+        upkeep = _upkeep;
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            Modifiers 
+    //////////////////////////////////////////////////////////////*/
+    modifier onlyUpkeep(){
+        require(msg.sender == upkeep, "Only upkeep allowed");
+        _;
+    }
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Only owner allowed");
+        _;
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        Internal functions
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Called in `createAccount`. Initializes the account contract created in `createAccount`.
+    function _initializeAccount(address _account, address _admin, bytes calldata _data) internal override {
+        Account(payable(_account)).initialize(_admin, _data);
+    }
+
+    /// @dev Returns whether contract metadata can be set in the given execution context.
+    function _canSetContractURI() internal view virtual override returns (bool) {
+        return hasRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    /// @notice Returns the sender in the given execution context.
+    function _msgSender() internal view override(Multicall, Permissions) returns (address) {
+        return msg.sender;
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        Chainlink functions
+    //////////////////////////////////////////////////////////////*/
+
+    
+    /**
+    * @dev checkUpkeep function called off-chain by Chainlink Automation infrastructure
+    * @dev Checks for balances elegible for swap
+    * @return upkeepNeeded A boolean indicating whether upkeep is needed.
+    * @return performData The performData parameter triggering the performUpkeep
+    * @notice This function is external, view, and implements the Upkeep interface.
+    */
+    function checkUpkeep(bytes calldata ) external view  override  returns (bool upkeepNeeded, bytes memory performData) {
+        (upkeepNeeded, performData) = _checkUpkeep();
+    }
+    
+    function _checkUpkeep() internal view returns (bool, bytes memory){
+        
+        address[] memory swappableERC20 = s_swappableERC20;
+        address[] memory wallets = getAllAccounts();
+        address[] memory assetsToSwap = new address[](swappableERC20.length);
+        address[] memory filteredAssetsToSwap;
+        uint count;
+        for (uint i; i<wallets.length; ++i){
+            for (uint j; j<swappableERC20.length; ++j){
+                if (IERC20(swappableERC20[j]).balanceOf(wallets[i])>0){
+                    assetsToSwap[count] = swappableERC20[j];
+                    ++count;
+                }
+            }
+            if (assetsToSwap.length >0){
+                filteredAssetsToSwap = new address[](count);
+                for (uint k; k<count; ++k){
+                    filteredAssetsToSwap[k] = assetsToSwap[k];
+                }
+                return  (true, abi.encode(wallets[i], filteredAssetsToSwap));
+            }
+        }
+    }
+
+    /**
+    * @dev performUpkeep function called by Chainlink Automation infrastructure after checkUpkeep checks
+    * @param performData the data inputed by Chainlink Automation retrieved by checkUpkeep
+    */
+    function performUpkeep(bytes calldata performData) onlyUpkeep external override(AutomationCompatibleInterface) {
+        (address wallet, address[] memory assetsToSwap) = abi.decode(performData, (address, address[]));
+        for (uint i; i< assetsToSwap.length; ++i){
+            uint balance = IERC20(assetsToSwap[i]).balanceOf(wallet);
+            Account(payable(wallet)).executeSwapAndSupply(0, 0, assetsToSwap[i], address(123));
+        } 
+    }
+}
