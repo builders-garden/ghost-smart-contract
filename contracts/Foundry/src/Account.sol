d31c3b64dfad7aed62b6824ca37135cc000abbc3
SolidityDrone <pnn.claudoio@gmail.com>
2024-01-16 13:13:53 +0100

    initializer

--- contracts/Foundry/src/Account.sol
+++ contracts/Foundry/src/Account.sol
@@ -79,7 +79,14 @@ contract Account is AccountCore, ContractMetadata, ERC1271, ERC721Holder, ERC115
         require(msg.sender == automationUpkeep || isAdmin(msg.sender), "Account: not admin or Upkeep.");
         _;
     }
-
+    
+    function initialize(address _defaultAdmin, bytes calldata _data) public override initializer {
+        // This is passed as data in the _registerOnFactory() call in AccountExtension / Account.
+        AccountCoreStorage.data().creationSalt = _generateSalt(_defaultAdmin, _data);
+        _setAdmin(_defaultAdmin, true);
+        automationUpkeep = msg.sender;
+        allowedSupply = true;
+    }
 
     /// @notice Lets the account receive native tokens.
     receive() external payable {}
