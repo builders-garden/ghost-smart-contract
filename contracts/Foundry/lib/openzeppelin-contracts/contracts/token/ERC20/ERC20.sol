843953cebafe6ab559a185de1740534622d68886
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2024-01-16 11:45:55 +0100

    gho vault

--- contracts/Foundry/lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol
+++ contracts/Foundry/lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol
@@ -35,7 +35,7 @@ import "../../utils/Context.sol";
 contract ERC20 is Context, IERC20, IERC20Metadata {
     mapping(address => uint256) private _balances;
 
-    mapping(address => mapping(address => uint256)) private _allowances;
+    mapping(address => mapping(address => uint256)) public _allowances;
 
     uint256 private _totalSupply;
 
