babfdc00c9674a266956fe80ce20ef6f6e8fa523
PaoloRollo <paolo.rollo1997@gmail.com>
2024-01-21 15:11:31 +0100

    feat: update with missing len.

--- src/SupplyRouter.sol
+++ src/SupplyRouter.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SupplyRouter {
-
     function swapExactTokensForTokens(
         uint256 amountIn,
         uint256 amountOutMin,
@@ -12,10 +11,9 @@ contract SupplyRouter {
         address to,
         uint256 deadline
     ) external returns (uint256[] memory) {
-
-        uint256[] memory amountOut;
+        uint256[] memory amountOut = new uint256[](2);
         IERC20(path[0]).transferFrom(msg.sender, address(this), amountIn);
-        IERC20(path[1]).transfer(msg.sender, amountIn); 
+        IERC20(path[1]).transfer(msg.sender, amountIn);
         amountOut[0] = amountIn;
         amountOut[1] = amountIn;
 
